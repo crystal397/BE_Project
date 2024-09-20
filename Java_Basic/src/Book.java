3d27ebf84d33f1cd56b4c7e0648b150926599742
Crystal <crystal397@cuk.edu>
2024-09-20 10:22:17 +0900

    create class

---
+++ Java_Basic/src/Book.java
@@ -0,0 +1,19 @@
+// class -> 새로운 자료형 설계하는 도구(Modeling)
+
+public class Book {
+    public String title;
+    public int price;
+    public String company;
+    public String author;
+    public int page;
+    public String isbn;
+}
+
+class Member {
+    public String name;
+    public int age;
+    public String tel;
+    public String mail;
+    public String addr;
+    public float weight;
+}
---
+++ Java_Basic/src/Book_MemberTest.java
@@ -0,0 +1,22 @@
+public class Book_MemberTest {
+    public static void main(String[] args) {
+        // 객체(인스턴스) 생성
+        Book b = new Book();
+        b.title="java";
+        b.price=30000;
+        b.company="fastcampus";
+        b.author="crystal";
+        b.page=700;
+        b.isbn="1199110";
+        System.out.println(b.title+"\t"+b.price+"\t"+b.company+"\t"+b.author+"\t"+b.page+"\t"+b.isbn);
+
+        Member m = new Member();
+        m.name="홍길동";
+        m.age=30;
+        m.tel="010-111-1111";
+        m.mail="fc@naver.com";
+        m.addr="Seoul";
+        m.weight=57.6f;
+        System.out.println(m.name+"\t"+m.age+"\t"+m.tel+"\t"+m.mail+"\t"+m.addr+"\t"+m.weight);
+    }
+}
