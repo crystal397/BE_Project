882e3a9461218907498bc402abcc7ca913a72528
최수정 <crystal@choesujeongs-MacBook-Air.local>
2024-09-26 16:03:16 +0900

    char test

---
+++ Java_Basic/src/CharExam.java
@@ -0,0 +1,6 @@
+public class CharExam {
+    public static void main(String[] args) {
+        int sum = '1'-'0'+'2'-'0'+'3'-'0'+'4'-'0'+'5'-'0';
+        System.out.println("sum = " + sum);
+    }
+}
